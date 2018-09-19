c258dd24d1969771fd32f587ce6b17b467fceddf
zhangjingwei0512 <42828228+zhangjingwei0512@users.noreply.github.com>
2018-09-19 09:33:29 +0800

    further2

--- further2.py
+++ further2.py
@@ -11,13 +11,14 @@ with open('further2.csv','w',newline='')as f:
             if i==8 and j>9:
                 break
             else:
-                url='https://github.com/myersjustinc?tab=overview&from=201{year}-{month}-01&to=201{year}-{month}-31'.format(year=i,month=j)
+                url='https://github.com/myersjustinc?tab=overview&from=201{0}-{1:02d}-01&to=201{0}-{1:02d}-31'.format(i,j)
                 r=requests.get(url).text
                 data=BeautifulSoup(r,"html.parser")
-                month_1='201{0}/{1}'.format(i,j)
+                month_1='201{0}/{1:02d}'.format(i,j)
                 if data.find('span',attrs={'class':'text-gray m-0'})!=None:
                     detail=data.find('span',attrs={'class':'text-gray m-0'}).text.strip()
                     writer.writerow([month_1,detail])
+                    continue
                 if data.find('span',attrs={'class':'float-left'})!=None:
                     detail=data.find_all('button',attrs={'class':'btn-link f4 muted-link no-underline lh-condensed width-full js-details-target'})
                     for k in range(0,len(detail)):
